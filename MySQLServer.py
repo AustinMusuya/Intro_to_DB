62c9067e6254715879af9f4a51981f75abefa06e
AustinMusuya <amusuya57@gmail.com>
2024-10-18 13:25:04 +0300

    configure code in MYSQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,30 +1,27 @@
-"""
-Write a simple python script that creates the database alx_book_store in your MySQL server.
 
-Name of python script should be MySQLServer.py
-If the database alx_book_store already exists, your script should not fail
-You are not allowed to use the SELECT or SHOW statements
-NOTE :
-
-Required to print message such as Database 'alx_book_store' created successfully! when database is successfully created.
-
-Print error message to handle errors when failing to connect to the DB.
-
-handle open and close of the DB in your script.
-
-"""
 # download & import mysql-connector package
 import mysql.connector
-
+from mysql.connector import Error
 
 # set up connection with connect method
+try:
+    mydb = mysql.connector.connect(
+        host='localhost', user='root', password='Chrislil1!')
 
-mydb = mysql.connector.connect(
-    host='localhost', user='root', password='Chrislil1!', database='newdb')
+    # test the connection
 
-# test the connection
+    # print(f"Database {mydb.get_server_info()} created succesfully")
 
-print(f"Database {mydb.get_server_info()} created succesfully")
+    # write you sql queries/print to console
+    mycursor = mydb.cursor()
 
-# write you sql queries/print to console
-# close connection
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+    print(f"Database 'alx_bookstore' created succesfully!")
+except Error as e:
+    print(e)
+finally:
+    # close connection
+    mycursor.close()
+    mydb.close()
+    print("MySQL connection closed.")
