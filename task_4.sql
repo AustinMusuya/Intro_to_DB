beec273d353281626cebec353bf6101c19271455
AustinMusuya <amusuya57@gmail.com>
2024-10-18 13:02:08 +0300

    configure query in 'task_4.sql'

--- task_4.sql
+++ task_4.sql
@@ -11,4 +11,15 @@ All SQL keywords should be in uppercase
 
 USE alx_book_store;
 
-SELECT * FROM Books;
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    COLUMN_KEY, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
